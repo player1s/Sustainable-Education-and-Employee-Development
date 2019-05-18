7815ee166c99cba0c0b6474e1757ab24c15e98c3
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-05-18 14:58:31 +0200

    added docs, removed firebase elements

--- src/main/java/SEED/Controller/CompanyController.java
+++ src/main/java/SEED/Controller/CompanyController.java
@@ -2,6 +2,7 @@ package SEED.Controller;
 
 import SEED.Data.CompanyRepository;
 import SEED.Model.Company;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +10,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Controller
@@ -17,17 +26,23 @@ public class CompanyController {
 
 
     private CompanyRepository companyRepository = new CompanyRepository();
-/*
-    @RequestMapping("/Company")
-    public String listCompanies(ModelMap modelMap) {
-        List<Company> allCompanies = companyRepository.getAllElements();
-        modelMap.put("Companies", allCompanies);
-        return "home";
+
+    
+    public CompanyController() throws IOException {
     }
-*/
+
+    /*
+        @RequestMapping("/Company")
+        public String listCompanies(ModelMap modelMap) {
+            List<Company> allCompanies = companyRepository.getAllElements();
+            modelMap.put("Companies", allCompanies);
+            return "home";
+        }
+    */
     @RequestMapping("/Company")
     public String listCompanies(Model model) {
         model.addAttribute("companies", companyRepository.getAllElements());
+
         return "home";
     }
 
